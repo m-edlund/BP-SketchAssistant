28748510286f10e9b57dea91d3e1650f8d4dbe41
Martin Edlund <*lennyface*>
2018-11-11 21:25:57 +0100

    Started Userstory 2, filedialogue in place, SVG files cannot be read corretly

--- SketchAssistant/SketchAssistant/Form1.cs
+++ SketchAssistant/SketchAssistant/Form1.cs
@@ -20,8 +20,28 @@ namespace SketchAssistant
             InitializeComponent();
         }
 
+        //Dialog to select a file.
+        OpenFileDialog ofd = new OpenFileDialog();
+        //Image loaded on the left
+        Image leftImage = null;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.DoubleBuffered = true;
+            //Connect the Paint event of the left picture box to the event handler method.
+            pictureBoxLeft.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxLeft_Paint);
+        }
+
+        private void pictureBoxLeft_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            //Create a local version of the graphics object
+            //Graphics g = e.Graphics;
+
+            //Draw something
+            if(leftImage != null)
+            {
+                pictureBoxLeft.Image = leftImage;
+            }
 
         }
 
@@ -38,6 +58,42 @@ namespace SketchAssistant
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //Load button, will open an OpenFileDialog
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "SVG|*.svg;*.jpg";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                toolStripLoadStatus.Text = ofd.SafeFileName;
+                Svg.SvgDocument svgDoc = Svg.SvgDocument.Open(ofd.FileName);
+                leftImage = new Bitmap(svgDoc.Draw());
+                //leftImage = Image.FromFile(ofd.FileName);
+                //The following line is needed, as else on first image load
+                //the image will only be shown after resizing the window.
+                this.Refresh();
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBoxLeft.Update();
         }
     }
 }
---
+++ userstory2.md
@@ -0,0 +1,13 @@
+# Userstory 2  
+
+|**ID**|2|
+|-|-|
+|**Name**|Lademodul|
+|**Beschreibung**|Man kann einen Knopf in der Toolbar drücken, um einen Dateimanager zu öffnen, welcher einem erlaubt Vektorgrafiken auszuwählen, welche im linken Bereich des Hauptfensters angezeigt werden.|
+|**Akzeptanzkriterium**|Der Knopf wird korrekt in der Toolbar angezeigt und es wird ein Dateimanager angezeigt, wenn er gedrückt wird. Im Dateimanager können nur Vektorgrafiken ausgewählt werden. Drückt man im Dateimanager auf 'Abbrechen' so wird der Ladevorgang abgebrochen und der Nutzer kehrt zum Hauptfenster zurück ohne eine Grafik zu laden. Kann die Datei nicht geladen werden, wird ein Fenster angezeigt welches den Nutzer informiert, dass die Datei nicht geladen werden konnte, sowie den Fehler. Falls die Datei geladen werden kann, wird sie im linken Bereich des Hauptfensters angezeigt. Ist die Grafik nicht gleich groß wie der Bereich wird sie trotzdem korrekt angezeigt. Die Größe des Bereichs wird bei kleineren Grafiken verkleinert, jedoch bei größeren nicht vergrößert (Der Bereich muss immer die Hälfte des Hauptfensters einnehmen). Die Grafik wird bei entsprechend skaliert, sodass sie in ihrer Gesamtheit im linken Bereich zu sehen ist.|
+|Geschätzter Aufwand (Story Points)|8|
+|Entwickler|Martin|
+|Umgesetzt in Iteration|keine|
+|Tatsächlicher Aufwand (Std.)|keine|
+|Velocity (Std./Story Point)|keine|
+|Bemerkungen|Keine|
