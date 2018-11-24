6ae9bcee5e8704670f6e6251e454b3faf2d1eb61
Martin Edlund <*lennyface*>
2018-11-24 16:21:55 +0100

    Fleshed out Line class

--- SketchAssistant/SketchAssistant/Line.cs
+++ SketchAssistant/SketchAssistant/Line.cs
@@ -9,12 +9,38 @@ namespace SketchAssistant
 {
     class Line
     {
-        private Point Start { get; set; }
-        private Point End { get; set; }
+        private List<Point> linePoints;
 
-        public Line(int[] points)
+        public Line(List<Point> points)
         {
+            linePoints = new List<Point>(points);
+        }
 
+        public Point GetStartPoint()
+        {
+            return linePoints.First();
+        }
+
+        public Point GetEndPoint()
+        {
+            return linePoints.Last();
+        }
+
+        /// <summary>
+        /// A function that takes a Graphics element and returns it with
+        /// the line drawn on it.
+        /// </summary>
+        /// <param name="canvas">The Graphics element on which the line shall be drawn</param>
+        /// <returns>The given Graphics element with the additional line</returns>
+        public Graphics DrawLine(Graphics canvas)
+        {
+            Pen thePen = new Pen(Color.Black);
+            for(int i = 0; i < linePoints.Count; i++)
+            {
+                canvas.DrawLine(thePen, linePoints[i], linePoints[i + 1]);
+            }
+            canvas.DrawLine(thePen, linePoints[linePoints.Count-1], linePoints[linePoints.Count]);
+            return canvas;
         }
     }
 }
