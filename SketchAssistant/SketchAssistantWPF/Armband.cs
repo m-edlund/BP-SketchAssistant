48ab2050ccb3f80cbfc69c8d6756be4d99ccd9a1
Rumei <rumei.ma@gmail.com>
2019-02-26 17:15:33 +0100

    added Armband dummy

---
+++ SketchAssistant/SketchAssistantWPF/Armband.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SketchAssistantWPF
+{
+    internal class Armband
+    {
+        internal void pushForward()
+        {
+            Console.WriteLine("FORWARD_PUSH");
+        }
+
+        internal void pushBackward()
+        {
+            Console.WriteLine("BACKWARD_PUSH");
+        }
+    }
+}
